961e14916184ef120150a6b2bf7105eb4536937c
JacksPC\Jack <jtillotson13@georgefox.edu>
2017-04-12 18:03:21 -0700

    no message

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by Jack on 12/22/2016.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+}
